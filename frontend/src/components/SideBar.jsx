e3e2157217bbf2c20430e87d2bfbd2f5fdcf4a02
Swarnim <swarnim@accurateic.in>
2025-01-08 10:09:48 +0530

    [feature] added visual state to show active page in sidebar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -7,23 +7,46 @@ const SideBar = () => {
       <li>
         <NavLink
           to="/"
-          className={({ isActive }) => isActive ? "active" : ""}
+          className={({ isActive }) =>
+            isActive ? "active bg-base-content text-base-300" : ""
+          }
           end
-        >Dashboard</NavLink>
+        >
+          Dashboard
+        </NavLink>
       </li>
       <li>
         <NavLink
           to="/engine"
-          className={({ isActive }) => isActive ? "active" : ""}
+          className={({ isActive }) =>
+            isActive ? "active bg-base-content text-base-300" : ""
+          }
           end
-        >Engine</NavLink>
+        >
+          Engine
+        </NavLink>
       </li>
       <li>
         <NavLink
           to="/generator"
-          className={({ isActive }) => isActive ? "active" : ""}
+          className={({ isActive }) =>
+            isActive ? "active bg-base-content text-base-300" : ""
+          }
           end
-        >Generator</NavLink>
+        >
+          Generator
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/reports"
+          className={({ isActive }) =>
+            isActive ? "active bg-base-content text-base-300" : ""
+          }
+          end
+        >
+          Reports
+        </NavLink>
       </li>
       <li>
         <details open>
@@ -32,27 +55,37 @@ const SideBar = () => {
             <li>
               <NavLink
                 to="/testchart"
-                className={({ isActive }) => isActive ? "active" : ""}
+                className={({ isActive }) =>
+                  isActive ? "active bg-base-content text-base-300" : ""
+                }
                 end
               >
                 TestChart
               </NavLink>
             </li>
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
+            <li>
+              <a>Submenu 1</a>
+            </li>
+            <li>
+              <a>Submenu 2</a>
+            </li>
             <li>
               <details open>
                 <summary>Parent</summary>
                 <ul>
-                  <li><a>Submenu 2</a></li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
                 </ul>
               </details>
             </li>
           </ul>
         </details>
       </li>
-      <li><a>Item 3</a></li>
-    </ul >
+      <li>
+        <a>Item 3</a>
+      </li>
+    </ul>
   );
 };
 
