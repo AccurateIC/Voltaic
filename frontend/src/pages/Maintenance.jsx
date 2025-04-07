1a1064517a61ab4103c1d1bf3a97924ef4c73ba6
swarnim-accurateic <swarnim@accurateic.in>
2025-04-07 14:25:04 +0530

    feat(pdm): use chartjs for smooth transitions & added features

--- frontend/src/pages/Maintenance.jsx
+++ frontend/src/pages/Maintenance.jsx
@@ -1,18 +1,32 @@
 import { useState, useEffect } from "react";
 import { CheckCircle, XCircle } from "lucide-react";
-import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, Legend } from "recharts";
 import { toast } from "sonner";
 import { DateTime } from "luxon";
 import { useMessageBus } from "../lib/MessageBus.js";
 import { cn } from "../lib/Utils.js";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import "chartjs-adapter-luxon";
+
+ChartJS.register(CategoryScale, TimeScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const StatusCard = ({ isLoading, title, isError, errorMessage, disabled }) => {
   return (
-
-    <div className={cn(
-      "w-full h-18 shadow-sm flex flex-row rounded items-center p-4",
-      disabled ? "text-gray-600 bg-gray-400" : "text-base-content bg-base-200"
-    )}>
+    <div
+      className={cn(
+        "w-full h-18 shadow-sm flex flex-row rounded items-center p-4",
+        disabled ? "text-gray-600 bg-gray-400" : "text-base-content bg-base-200"
+      )}>
       <div className="font-bold flex flex-row space-x-2">
         {isLoading ? (
           <>
@@ -32,85 +46,178 @@ const StatusCard = ({ isLoading, title, isError, errorMessage, disabled }) => {
           </div>
         ) : null}
       </div>
-    </div >
+    </div>
   );
 };
 
+// {
+//   "id": 1,
+//   "timestamp": "2025-03-27T07:21:22.000Z",
+//   "sensorPropertyId": 1,
+//   "value": 0,
+//   "maintenanceNotificationId": null,
+//   "pdmDataKindId": 1,
+//   "createdAt": "2025-04-02T11:13:03.418+00:00",
+//   "updatedAt": "2025-04-02T11:13:03.418+00:00",
+//   "sensorProperty": {
+//     "id": 1,
+//     "propertyName": "vibration_acceleration_x",
+//     "unit": "g",
+//     "createdAt": "2025-04-02T11:12:51.769+00:00",
+//     "updatedAt": "2025-04-02T11:12:51.770+00:00"
+//   },
+//   "pdmDataKind": {
+//     "id": 1,
+//     "kind": "actual",
+//     "createdAt": "2025-04-02T11:12:51.763+00:00",
+//     "updatedAt": "2025-04-02T11:12:51.763+00:00"
+//   }
+// }
+
+const PdmGraph = ({ actualPdmData, forecastedPdmData }) => {
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: { position: "top" },
+      title: {
+        display: true,
+        text: "Vibration Sensor Data",
+        color: "#fff",
+        font: {
+          size: 18,
+          weight: "bold",
+        },
+      },
+      tooltip: {},
+    },
+    scales: {
+      x: {
+        type: "time",
+        position: "bottom",
+        time: {
+          unit: "minute", // or "hour", "day", etc.
+          tooltipFormat: "yyyy-MM-dd HH:mm:ss",
+          displayFormats: {
+            minute: "HH:mm",
+            hour: "MMM d, HH:mm",
+            day: "MMM d",
+          },
+        },
+        title: {
+          display: true,
+          text: "Timestamp",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Vibration Acceleration (g-units)",
+        },
+      },
+    },
+  };
+
+  const data = {
+    datasets: [
+      // ACTUAL
+      {
+        label: "Actual Vibration Data",
+        data: actualPdmData.map((item) => ({
+          // x: new Date(item.timestamp).getTime(),
+          x: DateTime.fromISO(item.timestamp).toJSDate(),
+
+          y: item.value,
+        })),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        pointStyle: "circle",
+        pointHoverRadius: 5,
+        pointRadius: 0,
+        pointHitRadius: 10,
+      },
+      // FORECASTED
+      {
+        label: "Forecasted Vibration Data",
+        data: forecastedPdmData.map((item) => ({
+          // x: new Date(item.timestamp).getTime(),
+          x: DateTime.fromISO(item.timestamp).toJSDate(),
+          y: item.value,
+        })),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        pointStyle: "circle",
+        pointHoverRadius: 5,
+        pointRadius: 0,
+        pointHitRadius: 10,
+      },
+    ],
+  };
+
+  return <Line options={options} data={data} />;
+};
+
 const Maintenance = () => {
-  const [pdmData, setPdmData] = useState([]);
-  const [pdmDataForGraph, setPdmDataForGraph] = useState([]);
+  const [actualPdmData, setActualPdmData] = useState([]);
+  const [forecastedPdmData, setForecastedPdmData] = useState([]);
+
   const [isPdmLoading, setIsPdmLoading] = useState(true);
   const [isPdmError, setIsPdmError] = useState(false);
   const [pdmErrorMessage, setPdmErrorMessage] = useState("");
 
   // fetch pdmData from localstorage
   useEffect(() => {
-    fetchPdmVibrationData()
+    fetchPdmVibrationData();
     // const pdmDataString = localStorage.getItem("pdmData");
     // setPdmData(JSON.parse(pdmDataString));
   }, []);
 
-
   const fetchPdmVibrationData = async () => {
     try {
-      setIsPdmLoading(true)
-      const response = await fetch(`${import.meta.env.VITE_ADONIS_BACKEND}/pdm/getRecent`, {
+      setIsPdmLoading(true);
+      // fetch actual data
+      const response = await fetch(`${import.meta.env.VITE_ADONIS_BACKEND}/pdm/getRecentActual`, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
-        credentials: "include"
-      })
+        credentials: "include",
+      });
 
       if (!response.ok) {
         const errorData = await response.json();
-        throw new Error(errorData.message || `Failed to fetch pdm data`)
+        throw new Error(errorData.message || `Failed to fetch pdm data`);
       }
 
       const data = await response.json();
-      console.log("PDM DATAAA", data);
-      setPdmData(data);
-    } catch (err) {
-      console.error(err)
-      toast.error(`Failed to fetch vibration data`)
-    } finally {
-      setIsPdmLoading(false)
-    }
-  }
+      setActualPdmData(data);
 
+      // fetch forecasted data
+      const forecastedResponse = await fetch(`${import.meta.env.VITE_ADONIS_BACKEND}/pdm/getRecentForecasted`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+
+      if (!forecastedResponse.ok) {
+        const errorData = await forecastedResponse.json();
+        throw new Error(errorData.message || `Failed to fetch pdm data`);
+      }
+
+      const forecastedData = await forecastedResponse.json();
+      setForecastedPdmData(forecastedData);
+    } catch (err) {
+      console.error(err);
+      toast.error(`Failed to fetch vibration data`);
+    } finally {
+      setIsPdmLoading(false);
+    }
+  };
 
   useMessageBus("pdm", (message) => {
     console.log("new pdm data received liveeee");
     // setPdmData(JSON.parse(localStorage.getItem("pdmData")));
-
     // fetch data from database
-    fetchPdmVibrationData()
-
+    fetchPdmVibrationData();
   });
-
-  useEffect(() => {
-    console.log("pdm data changed", pdmData);
-    if (!pdmData || !Array.isArray(pdmData) || pdmData.length === 0) {
-      toast.error("No Predictive Maintenance Data available.");
-      return;
-    }
-    // transform data for plotting graph
-    const formattedData = pdmData.map(item => {
-      return {
-        timestamp: item.timestamp,
-        value: item.value,
-        actual: item.pdmDataKind.kind === 'actual' ? item.value : null,
-        forecast: item.pdmDataKind.kind === 'forecasted' ? item.value : null,
-        sensorProperty: item.sensorProperty.propertyName,
-        unit: item.sensorProperty.unit
-      }
-    })
-
-    formattedData.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
-
-
-    setPdmDataForGraph(formattedData);
-
-    setIsPdmLoading(false);
-  }, [pdmData]);
 
   return (
     <div className="flex flex-col w-full h-full">
@@ -125,55 +232,20 @@ const Maintenance = () => {
             errorMessage={pdmErrorMessage}
             disabled={false}
           />
+          {/*
           <StatusCard isLoading={isPdmLoading} title={`Temperature`} isError={false} errorMessage={``} disabled={true} />
-          <StatusCard isLoading={isPdmLoading} title={`Hydrocarbon Emission`} isError={false} errorMessage={``} disabled={true} />
+          <StatusCard
+            isLoading={isPdmLoading}
+            title={`Hydrocarbon Emission`}
+            isError={false}
+            errorMessage={``}
+            disabled={true}
+          />
+          */}
         </div>
       </div>
       <div className="flex-1 min-h-0">
-        {pdmDataForGraph.length > 0 && (
-          <div className="h-full">
-            <ResponsiveContainer width="100%" height="100%">
-              <LineChart data={pdmDataForGraph}
-                margin={{ top: 5, right: 30, left: 20, bottom: 25 }} // Add bottom margin
-              >
-                <CartesianGrid strokeDasharray="3 3" stroke="#ccc" />
-                <XAxis
-                  dataKey="timestamp"
-                  stroke="#fff"
-                  tickFormatter={(timestamp) => DateTime.fromISO(timestamp).toFormat("HH:mm:ss")}
-                  label={{ value: "Timestamp", position: "insideBottom", offset: -10 }}
-                />
-                <YAxis stroke="#fff" label={{ value: "Vibration (G-Units)", position: "insideLeft", angle: -90 }} />
-                <Tooltip
-                  contentStyle={{ backgroundColor: "#333", border: "none", color: "#fff" }}
-                  labelFormatter={(timestamp) => DateTime.fromISO(timestamp).toFormat("HH:mm:ss")}
-                />
-                <Line
-                  type="monotone"
-                  dataKey="actual"
-                  stroke="#ff7300"
-                  strokeWidth={2}
-                  dot={false}
-                  name="Actual"
-                  connectNulls
-                />
-                {/*
-                */}
-                <Line
-                  type="monotone"
-                  dataKey="forecast"
-                  stroke="#8884d8"
-                  strokeWidth={2}
-                  // strokeDasharray="5 5" // This creates the dotted/dashed line
-                  dot={false}
-                  name="Forecast"
-                  connectNulls
-                />
-                <Legend verticalAlign="top" iconType="diamond" height={36} />
-              </LineChart>
-            </ResponsiveContainer>
-          </div>
-        )}
+        {actualPdmData.length > 0 && <PdmGraph actualPdmData={actualPdmData} forecastedPdmData={forecastedPdmData} />}
       </div>
     </div>
   );
