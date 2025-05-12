c036a5173f960d72cc6ce44969d1bc6fdd482e79
CodeByAnkitaM <ankita@accurateic.in>
2025-05-12 16:59:06 +0530

    Fixed: Added Min and Max Timestamp Range on X-axis

--- frontend/src/components/charts/AnomalyGraphModal.jsx
+++ frontend/src/components/charts/AnomalyGraphModal.jsx
@@ -1,72 +1,79 @@
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement, Tooltip, Legend } from 'chart.js';
-import { DateTime } from 'luxon';
-import { useEffect } from 'react';
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement, Tooltip, Legend } from "chart.js";
+import { DateTime } from "luxon";
+import { useEffect } from "react";
 
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, Tooltip, Legend);
 
 const AnomalyGraphModal = ({ isOpen, onClose, graphData, selectedEntry }) => {
-    useEffect(()=>{
-        console.log(graphData);
-        console.log(selectedEntry);
-    })
+  useEffect(() => {
+    console.log(graphData);
+    console.log(selectedEntry);
+  });
   if (!isOpen) return null;
 
   // Prepare data for Chart.js
-const sortedData = [...graphData].sort((a, b) => a.x - b.x);
+  const sortedData = [...graphData].sort((a, b) => a.x - b.x);
 
-const chartData = {
-  labels: sortedData.map((point) => DateTime.fromMillis(point.x).toFormat('HH:mm:ss')),
-  datasets: [
-    {
-      label: 'Anomaly Event',
-      data: sortedData.map((point) => point.y),
-borderColor: "rgba(82, 120, 209, 1)",
+  const chartData = {
+    labels: sortedData.map((point) => DateTime.fromMillis(point.x).toFormat("HH:mm:ss")),
+    datasets: [
+      {
+        label: "Anomaly Event",
+        data: sortedData.map((point) => point.y),
+        borderColor: "rgba(82, 120, 209, 1)",
         backgroundColor: "rgba(82, 120, 209, 0.5)",
-      fill: false,
-      tension: 0.4,
-      pointRadius: 2,
-      pointBackgroundColor: '#ff0000',
-      pointBorderColor: '#ff0000',
-    },
-  ],
-};
+        fill: false,
+        tension: 0.4,
+        pointRadius: 2,
+        pointBackgroundColor: "#ff0000",
+        pointBorderColor: "#ff0000",
+      },
+    ],
+  };
 
   const chartOptions = {
     responsive: true,
     maintainAspectRatio: false,
     scales: {
       x: {
+        type: "timeseries",
+        position: "bottom",
+
+        min: DateTime.fromMillis(sortedData[0].x).minus({ minutes: 2.5 }).toISO(),
+        max: DateTime.fromMillis(sortedData[0].x).plus({ minutes: 2.5 }).toISO(),
         title: {
           display: true,
-          text: 'Timestamp',
-          color: '#fff',
+          text: "Timestamp",
+          color: "#fff",
         },
         ticks: {
-          color: '#fff',
+          color: "#fff",
         },
         grid: {
-          color: '#444',
+          color: "#444",
         },
       },
       y: {
         title: {
           display: true,
-          text: `${selectedEntry?.archive?.gensetProperty?.readablePropertyName || "Property"} (${selectedEntry?.archive?.gensetProperty?.physicalQuantity?.unitSymbol || "unit"})`,
-          color: '#fff',
+          text: `${selectedEntry?.archive?.gensetProperty?.readablePropertyName || "Property"} (${
+            selectedEntry?.archive?.gensetProperty?.physicalQuantity?.unitSymbol || "unit"
+          })`,
+          color: "#fff",
         },
         ticks: {
-          color: '#fff',
+          color: "#fff",
         },
         grid: {
-          color: '#444',
+          color: "#444",
         },
       },
     },
     plugins: {
       legend: {
         labels: {
-          color: '#fff',
+          color: "#fff",
         },
       },
       tooltip: {
