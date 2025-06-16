c30d8c203cac3335be9ac68227c799b61ea0677f
swarnim-accurateic <swarnim@accurateic.in>
2025-06-16 12:40:11 +0530

    fix: round off RUL

--- frontend/src/pages/RUL.tsx
+++ frontend/src/pages/RUL.tsx
@@ -114,7 +114,7 @@ const RUL = () => {
             "rounded hover:bg-base-100 hover:text-base-content transition-all duration-200 shadow"
           )}>
           <div className="text-2xl font-semibold">Remaining Useful Life</div>
-          <div className="text-xl">{apiPoint[0]?.Remaining_Useful_Life || "N/A"} hours</div>
+          <div className="text-xl">{parseInt(apiPoint[0]?.Remaining_Useful_Life) || "N/A"} hours</div>
         </div>
       </div>
     </div>
