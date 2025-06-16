30ecd34c676301e28656591dc9d8826e65d4d96d
swarnim-accurateic <swarnim@accurateic.in>
2025-06-16 12:41:51 +0530

    fix: crash when point.day was undefined

--- frontend/src/components/charts/reports/PDMNotificationStatistics.tsx
+++ frontend/src/components/charts/reports/PDMNotificationStatistics.tsx
@@ -46,11 +46,9 @@ export const PDMNotificationStatistics = ({ timeDuration }: { timeDuration: Date
   switch (timeDuration) {
     case "week":
       xs = chartData.map((point) => {
-        if (point?.day)
+        if (point.day)
           return DateTime.fromObject({ year: point.year, month: point.month, day: point.day }).toFormat("ccc, MMM d");
-        else {
-          return null;
-        }
+        else throw new Error(`day not defined`);``
       });
       ys = chartData.map((point) => point.count);
       break;
