a17bd8cd8fe39cad135be141a1798b6245199b46
swarnim-accurateic <swarnim@accurateic.in>
2025-07-16 14:39:13 +0530

    fix: include credentials in another way

--- apps/frontend/src/lib/Tuyau.ts
+++ apps/frontend/src/lib/Tuyau.ts
@@ -7,7 +7,9 @@ import { BACKEND_BASE_URL } from "../config/backend";
 export const tuyau = createTuyau({
   api,
   baseUrl: BACKEND_BASE_URL,
-  headers: { "timezone": Intl.DateTimeFormat().resolvedOptions().timeZone },
-  credentials: "include", // TODO: does sending credentials in all requests result in failed requests
+  headers: { timezone: Intl.DateTimeFormat().resolvedOptions().timeZone },
   plugins: [superjson()],
+  fetch: (url, options = {}) => {
+    return fetch(url, { ...options, credentials: "include" });
+  },
 });
